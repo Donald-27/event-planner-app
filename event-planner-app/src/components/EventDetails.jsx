a1d7776cf48367ca65df97012b77ef4a2069d701
Donald Kiprop <kipropdonald27@gmail.com>
2025-04-21 17:10:05 +0300

    Add event description to EventDetails

--- event-planner-app/src/components/EventDetails.jsx
+++ event-planner-app/src/components/EventDetails.jsx
@@ -6,6 +6,7 @@ function EventDetails({ event }) {
       <h2>{event.title}</h2>
       <p>{event.date} at {event.time}</p>
       <p>{event.location}</p>
+      <p>{event.description}</p>
     </div>
   );
 }
