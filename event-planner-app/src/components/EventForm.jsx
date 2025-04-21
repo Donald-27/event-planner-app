d521289c6e58748c33fa92121fde441e572e3021
Donald Kiprop <kipropdonald27@gmail.com>
2025-04-21 17:11:59 +0300

    Added Event Title section in EventForm component

---
+++ event-planner-app/src/components/EventForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventTitle() {
+  return (
+    <label>
+      Event Title:
+      <input type="text" name="title" />
+    </label>
+  );
+}
+
+export default EventTitle;
